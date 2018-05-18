4e97c48b3262db9cdbc390b2816cdbcb3c73cc6b
sgc88 <sehribancheng@yahoo.com>
2018-05-17 23:49:39 -0700

    testing reset button

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function shuffle(array) {
 
     return array;
 }
-alert("wolcome!");
+
 
 
 /*
@@ -37,3 +37,10 @@ alert("wolcome!");
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ var resetGame= document.getElementById("repeat");
+ resetGame.addEventListener("click", restart);
+
+ function restart() {
+     console.log("you clicked me");
+ }
